dc8da3b15d52a98ac33c9d0cfc51d4e359121538
Mark Heath <mark.heath@gmail.com>
2024-01-16 20:01:25 +0000

    adding the newpurchasewebhook

---
+++ NewPurchaseWebhook.cs
@@ -0,0 +1,31 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Pluralsight.AzureFuncs
+{
+    public class NewPurchaseWebhook
+    {
+        private readonly ILogger _logger;
+
+        public NewPurchaseWebhook(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<NewPurchaseWebhook>();
+        }
+
+        [Function(nameof(NewPurchaseWebhook))]
+        public HttpResponseData Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route="purchase")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString("Welcome to Azure Functions!");
+
+            return response;
+        }
+    }
+}
