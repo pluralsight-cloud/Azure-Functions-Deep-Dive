d758b59b3fed9b07dffe0b369a584cc6db4fa550
Mark Heath <mark.heath@gmail.com>
2024-01-16 20:48:37 +0000

    after S03-L06 - accessing the request headers and body

--- NewPurchaseWebhook.cs
+++ NewPurchaseWebhook.cs
@@ -14,17 +14,38 @@ namespace Pluralsight.AzureFuncs
             _logger = loggerFactory.CreateLogger<NewPurchaseWebhook>();
         }
 
+        record NewOrderWebhook(int productId, int quantity, 
+            string customerName, string customerEmail, decimal purchasePrice);
+
         [Function(nameof(NewPurchaseWebhook))]
-        public HttpResponseData Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route="purchase")] HttpRequestData req)
+        public async Task<HttpResponseData> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route="purchase")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            //var body = await req.ReadAsStringAsync();
+            var order = await req.ReadFromJsonAsync<NewOrderWebhook>();
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString($"{order.customerName} purchased product {order.productId}!");
+
+            return response;
+        }
+
+        [Function(nameof(GetPurchase))]
+        public HttpResponseData GetPurchase(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route="purchase")] HttpRequestData req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
+            var userAgent = req.Headers.GetValues("User-Agent").FirstOrDefault() ?? "Unknown";
             var name = req.Query.Get("name") ?? "Anonymous";
-            response.WriteString($"Welcome {name}!");
+            response.WriteString($"Welcome {name} using {userAgent}!");
 
             return response;
         }
