f7f374152505d2fc6d4625122360f88c1b9e2d94
Mark Heath <mark.heath@gmail.com>
2024-03-23 13:29:43 +0000

    fan out fan in demo

--- NewOrderOrchestration.cs
+++ NewOrderOrchestration.cs
@@ -1,3 +1,4 @@
+using Azure.Storage.Blobs;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.DurableTask;
@@ -6,6 +7,12 @@ using Microsoft.Extensions.Logging;
 
 namespace Pluralsight.AzureFuncs
 {
+    public class OrderOrchestrationInput
+    {
+        public Guid OrderId { get; set; }
+        public string[] SeatNumbers { get; set; } = Array.Empty<string>();
+    }
+
     public static class NewOrderOrchestration
     {
         [Function(nameof(NewOrderOrchestration))]
@@ -13,24 +20,29 @@ namespace Pluralsight.AzureFuncs
             [OrchestrationTrigger] TaskOrchestrationContext context)
         {
             ILogger logger = context.CreateReplaySafeLogger(nameof(NewOrderOrchestration));
-            logger.LogInformation("Saying hello.");
-            var outputs = new List<string>();
+            logger.LogInformation("Creating tickets for new order.");
 
-            // Replace name and input with values relevant for your Durable Functions Activity
-            outputs.Add(await context.CallActivityAsync<string>(nameof(SayHello), "Tokyo"));
-            outputs.Add(await context.CallActivityAsync<string>(nameof(SayHello), "Seattle"));
-            outputs.Add(await context.CallActivityAsync<string>(nameof(SayHello), "London"));
+            var orderInput = context.GetInput<OrderOrchestrationInput>()!;
 
-            // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
-            return outputs;
+            var tasks = new List<Task<string>>();
+            foreach(var seat in orderInput.SeatNumbers)
+            {
+                tasks.Add(context.CallActivityAsync<string>(nameof(CreateTicket), seat));
+            }
+            var results = await Task.WhenAll(tasks);
+            return results.ToList();
         }
 
-        [Function(nameof(SayHello))]
-        public static string SayHello([ActivityTrigger] string name, FunctionContext executionContext)
+        [Function(nameof(CreateTicket))]
+        public static async Task<string> CreateTicket([ActivityTrigger] string seatNumber, 
+            [BlobInput("tickets", Connection ="AzureWebJobsStorage")] BlobContainerClient ticketsClient,
+            FunctionContext executionContext)
         {
-            ILogger logger = executionContext.GetLogger("SayHello");
-            logger.LogInformation("Saying hello to {name}.", name);
-            return $"Hello {name}!";
+            ILogger logger = executionContext.GetLogger("CreateTicket");
+            logger.LogInformation("Creating ticket for {seat}.", seatNumber);
+            // simulate taking time to create a ticket
+            await Task.Delay(5000);
+            return $"Ticket for seat {seatNumber}!";
         }
 
         [Function("NewOrderOrchestration_HttpStart")]
@@ -40,10 +52,15 @@ namespace Pluralsight.AzureFuncs
             FunctionContext executionContext)
         {
             ILogger logger = executionContext.GetLogger("NewOrderOrchestration_HttpStart");
+            var inputData = new OrderOrchestrationInput
+            {
+                OrderId = Guid.NewGuid(),
+                SeatNumbers = ["A1", "A2", "A3"]
+            };
 
             // Function input comes from the request content.
             string instanceId = await client.ScheduleNewOrchestrationInstanceAsync(
-                nameof(NewOrderOrchestration));
+                nameof(NewOrderOrchestration), inputData);
 
             logger.LogInformation("Started orchestration with ID = '{instanceId}'.", instanceId);
 
