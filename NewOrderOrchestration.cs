db1ddd792d2de808efa2da2b76a30ee10371d8d6
Mark Heath <mark.heath@gmail.com>
2024-03-28 20:08:43 +0000

    Wait for external approval demo

--- NewOrderOrchestration.cs
+++ NewOrderOrchestration.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using Azure.Storage.Blobs;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
@@ -11,10 +12,19 @@ namespace Pluralsight.AzureFuncs
     {
         public Guid OrderId { get; set; }
         public string[] SeatNumbers { get; set; } = Array.Empty<string>();
+        public decimal TotalPrice { get; set; }
+    }
+
+    public class ApprovalResult
+    {
+        public bool IsApproved { get; set; }
+        public string ApprovedBy { get;set; } = String.Empty;
     }
 
     public static class NewOrderOrchestration
     {
+        public const string OrderApprovalEventName = "OrderApproval";
+    
         [Function(nameof(NewOrderOrchestration))]
         public static async Task<List<string>> RunOrchestrator(
             [OrchestrationTrigger] TaskOrchestrationContext context)
@@ -24,6 +34,28 @@ namespace Pluralsight.AzureFuncs
 
             var orderInput = context.GetInput<OrderOrchestrationInput>()!;
 
+            if (orderInput.TotalPrice >= 500)
+            {
+                logger.LogInformation("Order is expensive, get manual approval");
+                await context.CallActivityAsync(nameof(RequestApproval), orderInput.OrderId);
+                try
+                {
+                    var result = await context.WaitForExternalEvent<ApprovalResult>(
+                        OrderApprovalEventName,
+                        TimeSpan.FromMinutes(1)
+                    );
+                    if (!result.IsApproved)
+                    {
+                        logger.LogWarning("Order was not approved");
+                        return [$"Order was not approved (approver: {result.ApprovedBy})"];
+                    }
+                }
+                catch (TaskCanceledException) 
+                {
+                    logger.LogWarning("Timed out waiting for approval");
+                    return ["Order Rejected - Timed Out waiting for approval"];
+                }
+            }
             var tasks = new List<Task<string>>();
             foreach(var seat in orderInput.SeatNumbers)
             {
@@ -45,28 +77,58 @@ namespace Pluralsight.AzureFuncs
             return $"Ticket for seat {seatNumber}!";
         }
 
+        [Function(nameof(RequestApproval))]
+        public static void RequestApproval([ActivityTrigger] Guid orderId, 
+            FunctionContext executionContext)
+        {
+            ILogger logger = executionContext.GetLogger("RequestApproval");
+            logger.LogInformation("Requesting Approval for {orderId}.", orderId);
+        }
+
         [Function("NewOrderOrchestration_HttpStart")]
         public static async Task<HttpResponseData> HttpStart(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
             [DurableClient] DurableTaskClient client,
             FunctionContext executionContext)
         {
+            
             ILogger logger = executionContext.GetLogger("NewOrderOrchestration_HttpStart");
+            var price = req.Query["price"] ?? "100";
             var inputData = new OrderOrchestrationInput
             {
                 OrderId = Guid.NewGuid(),
-                SeatNumbers = ["A1", "A2", "A3"]
+                SeatNumbers = ["A1", "A2", "A3"],
+                TotalPrice = Decimal.Parse(price)
             };
-
+            
             // Function input comes from the request content.
             string instanceId = await client.ScheduleNewOrchestrationInstanceAsync(
                 nameof(NewOrderOrchestration), inputData);
-
+            
             logger.LogInformation("Started orchestration with ID = '{instanceId}'.", instanceId);
 
             // Returns an HTTP 202 response with an instance management payload.
             // See https://learn.microsoft.com/azure/azure-functions/durable/durable-functions-http-api#start-orchestration
             return client.CreateCheckStatusResponse(req, instanceId);
+        }
+
+        [Function("NewOrderOrchestration_Approve")]
+        public static async Task<HttpResponseData> Approve(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req,
+            [DurableClient] DurableTaskClient client,
+            FunctionContext executionContext)
+        {
+            ILogger logger = executionContext.GetLogger("NewOrderOrchestration_Approve");
+            var approvalResult = await req.ReadFromJsonAsync<ApprovalResult>();
+            var id = req.Query["id"];
+            if (id == null || approvalResult == null)
+            {
+                return req.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            logger.LogInformation("Raising approval {approved} to {id}", approvalResult.IsApproved, id);
+            await client.RaiseEventAsync(id, OrderApprovalEventName, approvalResult);
+            return req.CreateResponse(HttpStatusCode.OK);
         }
     }
 }
